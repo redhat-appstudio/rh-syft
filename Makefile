4f9c9293676062e34a4684807745a1245d5d9668
Adam Cmiel <acmiel@redhat.com>
2024-01-16 16:29:04 +0100

    Implement and document midstream mechanisms
    Note that the Dockerfile is taken straight from the v0.89.0 upstream
    tag. 0.89.0 is the latest version that we can currently build
    hermetically in Konflux, though that should change soon.
    
    Modifications for the Konflux build process coming in a future commit.
    
    Signed-off-by: Adam Cmiel <acmiel@redhat.com>

---
+++ Makefile
@@ -0,0 +1,40 @@
+LAST_RELEASE = $(shell git describe --tags --abbrev=0 origin/redhat-latest)
+# either already released or a work in progress
+CURRENT_RELEASE = v0.89.0
+
+.PHONY: update-local
+update-local:
+	git fetch --tags origin redhat-latest
+	git fetch --tags upstream main
+
+.PHONY: list-upstream-versions
+list-upstream-versions:
+	# last 20 versions
+	@git tag | sort --version-sort | tail -n 20
+
+.PHONY: check-release
+check-release:
+	# latest upstream release
+	@git describe --tags --abbrev=0 upstream/main
+	# current version (either already released or a work in progress)
+	@echo $(CURRENT_RELEASE)
+	# last released downstream version
+	@echo $(LAST_RELEASE)
+
+.PHONY: check-build-changes
+check-build-changes:
+	git log --patch $(LAST_RELEASE)..$(CURRENT_RELEASE) -- Dockerfile .goreleaser.yaml
+
+.PHONY: generate-downstream
+generate-downstream:
+	hack/generate-downstream.sh -v $(CURRENT_RELEASE)
+
+.PHONY: backup-release-branch
+backup-release-branch:
+	git branch -f "redhat-$(LAST_RELEASE)" origin/redhat-latest
+	git push origin "redhat-$(LAST_RELEASE)"
+
+.PHONY: force-push-release-branch
+force-push-release-branch:
+	git branch -f redhat-latest $(CURRENT_RELEASE)
+	git push -f --tags origin redhat-latest
---
+++ hack/generate-downstream.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+set -o errexit -o nounset -o pipefail
+
+usage() {
+    cat << EOF
+Usage: $0 -v version_to_release [-f] [-m midstream_branch] [-b base_release_branch]
+
+Generate the downstream branch based on the specified upstream version.
+The downstream branch is named redhat-wip-\$version_to_release.
+
+-v: specify the upstream version to use as base for the downstream branch
+-m: specify the branch from which to apply the midstream modifications
+    default: current branch
+-b: specify the base release branch (from which to copy the .tekton/ folder)
+    default: redhat-latest
+-f: force-generate the downstream branch even if it already exists locally
+EOF
+}
+
+warn() {
+    echo "WARNING: $1" >&2
+}
+
+VERSION_TO_RELEASE=''
+MIDSTREAM_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+BASE_RELEASE_BRANCH=redhat-latest
+FORCE='false'
+
+CUSTOM_FILES=(
+    Dockerfile
+)
+
+while getopts v:m:b:fh opt; do
+    case "$opt" in
+        v) VERSION_TO_RELEASE=$OPTARG ;;
+        m) MIDSTREAM_BRANCH=$OPTARG ;;
+        b) BASE_RELEASE_BRANCH=$OPTARG ;;
+        f) FORCE='true' ;;
+        h) usage; exit 0 ;;
+        *) exit 1 ;;
+    esac
+done
+
+if [[ -z "$VERSION_TO_RELEASE" ]]; then
+    usage
+    exit 1
+fi
+
+wip_release_branch="redhat-wip-$VERSION_TO_RELEASE"
+if [[ "$FORCE" = 'true' ]]; then
+    git checkout -B "$wip_release_branch" "$VERSION_TO_RELEASE"
+else
+    if ! git checkout -b "$wip_release_branch" "$VERSION_TO_RELEASE"; then
+        echo "----------------------------------------------------------"
+        echo "If the $wip_release_branch branch already exists, you can:"
+        echo "- use '$0 -f ...' to overwrite it (discarding your changes)"
+        echo "- rename it ('git branch -m $wip_release_branch <backup_name>')"
+        exit 1
+    fi
+fi
+trap 'git checkout - >/dev/null' EXIT
+
+git rm -r .github/
+git commit -s -m "Remove unwanted CI setup"
+
+git checkout "$MIDSTREAM_BRANCH" -- "${CUSTOM_FILES[@]}"
+git add "${CUSTOM_FILES[@]}"
+git commit -s -m "Apply Red Hat specific modifications"
+
+if ! git fetch origin "$BASE_RELEASE_BRANCH"; then
+    warn "Could not fetch $BASE_RELEASE_BRANCH from origin. Will continue anyway."
+fi
+
+if git checkout "origin/$BASE_RELEASE_BRANCH" -- .tekton; then
+    git add .tekton
+    git commit -s -m "Copy Tekton pipelines from '$BASE_RELEASE_BRANCH' branch"
+else
+    warn "Could not copy Tekton pipelines from origin/$BASE_RELEASE_BRANCH."
+fi
+
+cat << EOF
+--------------------------------------------------------------------------------
+Generated downstream branch: $wip_release_branch
+--------------------------------------------------------------------------------
+EOF
